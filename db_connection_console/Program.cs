27ad0e83c9b5f6473c4b95a2fd6187b76fc4e9d9
jasaga2401 <jasaga@protonmail.com>
2023-11-09 22:51:45 +0000

    Add project files.

---
+++ db_connection_console/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Data Source=AMAZE\\SQLEXPRESS;Initial Catalog=person;Integrated Security=True";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connected to the database");
+
+                // Your database operations go here
+
+                // Example: Execute a query
+                string query = "SELECT * FROM dbo.tbl_customer";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Process the results
+                            Console.WriteLine($"Forename: {reader["forename"]}, Surname: {reader["surname"]}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
+
